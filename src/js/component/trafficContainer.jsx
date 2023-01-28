7b37cadad229fb35619ad0a3da3a9428e2948596
Carlos Herrera <c.herrerav97@gmail.com>
2023-01-28 04:36:29 +0000

    Mi primer commit

---
+++ src/js/component/trafficContainer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from 'react';
+
+
+export function TrafficLightContainer () {
+    const [colorOn, setColorOn] = useState("");
+    const colors = ["red", "yellow", "green"];
+    return (
+        <React.Fragment>
+            <div className="lightContainer bg-dark">
+                <div 
+                    className={"red light" + (colorOn == "red" ? " selected" : "")} 
+                    onClick={() => {return setColorOn("red")}}>
+                </div>
+                <div 
+                    className={"yellow light" + (colorOn == "yellow" ? " selected" : "")} 
+                    onClick={() => {return setColorOn("yellow")}}>
+                </div>
+                <div 
+                    className={"green light" + (colorOn == "green" ? " selected" : "")} 
+                    onClick={() => {return setColorOn("green")}}>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+                <button 
+                    type="button" 
+                    className="btn btn-info text-center" 
+                    onClick={() => {return setColorOn(colors[Math.floor(Math.random()*colors.length)])}}>
+                        Random Color
+                </button>
+            </div>
+        </React.Fragment>
+    )
+}
